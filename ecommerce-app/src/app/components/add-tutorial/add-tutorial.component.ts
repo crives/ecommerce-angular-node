012263635d663e10c8a49deabc47ae9961730f14
crives <car.rives@gmail.com>
2020-11-09 00:45:25 -0500

    create data service, components and app-routing module

--- ecommerce-app/src/app/components/add-tutorial/add-tutorial.component.ts
+++ ecommerce-app/src/app/components/add-tutorial/add-tutorial.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TutorialService } from 'src/app/services/tutorial.service';
 
+  // Service method to create Form to submit new tutorials
 @Component({
   selector: 'app-add-tutorial',
   templateUrl: './add-tutorial.component.html',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTutorialComponent implements OnInit {
 
-  constructor() { }
+  tutorial = {
+    title: '',
+    description: '',
+    published: false
+  };
+  submitted = false;
+
+  constructor(private tutorialService: TutorialService) { }
 
   ngOnInit(): void {
   }
 
+  saveTutorial() {
+    const data = {
+      title: this.tutorial.title,
+      description: this.tutorial.description
+    };
+
+    this.tutorialService.create(data)
+        .subscribe(
+          response => {
+            console.log(response);
+            this.submitted = true;
+          },
+          error => {
+            console.log(error);
+          });
+    }
+
+    newTutorial() {
+      this.submitted = false;
+      this.tutorial = {
+        title: '',
+        description: '',
+        published: false
+      };
+    }
 }
